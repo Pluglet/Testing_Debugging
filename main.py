8be6dfd8829ed57551f11ceec94a774932c12d2e
Alan Moody <alan_moody2002@yahoo.com.au>
2023-01-24 13:52:29 +0800

    second commit - debuggins started debugging section completed

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from typing import List
+
+
 def change_middle(my_list):
+    """
+    This function changes the middle number of a string
+    :param my_list:
+    :return:
+    """
     print('Start function')
     x = int(input('Enter number: '))
     my_list[1] = x
@@ -6,7 +14,7 @@ def change_middle(my_list):
 
 
 if __name__ == '__main__':
-    a = [0,1,2]
+    a: list[int] = [0, 1, 2]
     b = a
     change_middle(a)
     print(a)
